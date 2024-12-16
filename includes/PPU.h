5b5c847a7b0e464d8a05a9795f195ca9570ed9ed
bknote71 <bknote71@gmail.com>
2024-12-16 19:31:08 +0900

    implement sprite rendering

--- includes/PPU.h
+++ includes/PPU.h
@@ -2,6 +2,7 @@
 #define PPU_H
 
 #include <cstdint>
+#include <vector>
 
 enum PipelineState
 {
@@ -10,21 +11,59 @@ enum PipelineState
     PostRender,
 };
 
+/**
+ * NES PPU 하드웨어 특성상 렌더링 중에 VRAM/OAM을 쓰는 행위는 매우 위험
+ * - CPU는 이론상 언제든 IO 레지스터에 쓰기 가능하지만,
+ * - 안정적인 그래픽 출력을 위해서는 vblank 중(또는 렌더링 꺼진 상태)에 PPU 관련 IO 레지스터 접근을 하는 것이 관례.
+ */
+
 class PPU
 {
 public:
-    // [14-12]: fine Y, [11-10]: nametable, [9-5]: coarseY(타일 y축 위치), [4-0]: coarseX(타일 x축 위치)
-    uint16_t v;
+    // PPUCTRL
+    uint16_t baseNTAddr;  // 베이스 네임테이블주소 (0, 1, 2, 3)
+    bool vIncrement;      //
+    uint16_t sprPTAddr;   // 스프라이트 패턴테이블 주소 (0: $0000, 1: $1000) (8x8 일때 유효)
+    uint16_t bgPTAddr;    // 배경 패턴 테이블 주소 (0: $0000, 1: $1000)
+    uint8_t sprSize;      // 스프라이트 크기 (0: 8x8, 1: 8x16)
+    bool masterSlave;     // EXT 핀 관련
+    bool enableVblankNMI; // vblank NMI enable
+
+    // PPUMASK: 렌더링 제어 및 컬러 효과 제어
+    bool graycale;
+    bool showBgInLeftmost;
+    bool showSprInLeftmost;
+    bool enableBgRendering;
+    bool enableSprRendering;
+    uint8_t emphasizeRGB;
+
+    // PPUSTATUS
+    bool spriteOverflow;
+    bool sprZeroHit; // 특정 픽셀에서 스프라이트0과 배경이 겹치면 set.
+    bool vblank;
+
+    // Internal registers
+    uint16_t v; // [14-12]: fine Y, [11-10]: nametable, [9-5]: coarseY(타일 y축 위치), [4-0]: coarseX(타일 x축 위치)
     uint16_t t;
-    uint8_t bgPage;
-    uint8_t fineX; // pixel 단위의 세밀한 수평 스크롤(0~7)
+    uint8_t x; // fine X scroll
+    bool w;    // first or second write toggle
 
     uint32_t cycle;
     uint32_t scanline;
+    bool oddFrame;
 
     PipelineState pipelineState;
 
+    std::vector<uint32_t> oam;
+    std::vector<uint8_t> secondaryOAM; // 16개
+
+    std::vector<uint8_t> palette;
+    std::vector<std::vector<uint32_t>> pBuffer;
+
+    // methods
     uint8_t read(uint16_t address);
+    void setPPUCtrl(uint8_t ctrl);
+    void setPPUMask(uint8_t mask);
 
     void render();
     void preRender();
@@ -32,17 +71,18 @@ public:
     void postRender();
 
     void renderPixel();
-    void fetchBackgroundPixel(int x, int y, uint8_t &bgPixel, bool &bgOpaque);
-    void fetchSpritePixel(int x, int y, uint8_t &sprPixel, bool &sprOpaque, bool &sprForeground);
-    void compositePixel(int x, int y, uint8_t &bgPixel, uint8_t &sprPixel, bool &bgOpaque, bool &sprOpaque, bool &sprForeground);
+    void renderBackgroundPixel(int x, int y, uint8_t &bgPixel, bool &bgOpaque);
+    void renderSpritePixel(int x, int y, bool bgOpaque, uint8_t &sprPixel, bool &sprOpaque, bool &sprForeground);
+    uint8_t compositePixel(int x, int y, uint8_t bgPixel, uint8_t sprPixel, bool bgOpaque, bool sprOpaque, bool sprForeground);
 
     void clearFlags();
     uint8_t fetchNameTableData();
     uint8_t fetchAttributeTableData();
-    uint8_t fetchPatternTableLow(uint8_t tile, uint8_t fine_x);
-    uint8_t fetchPatternTableHigh(uint8_t tile, uint8_t fine_x);
+    uint8_t fetchPatternTablePixelData(uint16_t ptAddr, uint8_t tileX);
+    uint8_t fetchPatternTableLow(uint8_t tile, uint8_t tileX);
+    uint8_t fetchPatternTableHigh(uint8_t tile, uint8_t tileX);
 
-    void evaluateSpritesForNextScanline();
+    void evaluateSprites();
     void fetchSpriteTitleByte();
     void fetchSpriteAttributeByte();
     void fetchSpritePatternLow();
--- src/PPU.cpp
+++ src/PPU.cpp
@@ -1,7 +1,37 @@
 #include "PPU.h"
 
-static const int scanlineVisibleCycle = 256;
-static const int scanlineEndCycle = 256;
+static const int visibleCycle = 256;
+static const int endCycle = 256;
+
+// temp
+static const uint32_t colors[] = {
+    0x666666ff, 0x002a88ff, 0x1412a7ff, 0x3b00a4ff, 0x5c007eff, 0x6e0040ff, 0x6c0600ff, 0x561d00ff, 0x333500ff, 0x0b4800ff, 0x005200ff,
+    0x004f08ff, 0x00404dff, 0x000000ff, 0x000000ff, 0x000000ff, 0xadadadff, 0x155fd9ff, 0x4240ffff, 0x7527feff, 0xa01accff, 0xb71e7bff,
+    0xb53120ff, 0x994e00ff, 0x6b6d00ff, 0x388700ff, 0x0c9300ff, 0x008f32ff, 0x007c8dff, 0x000000ff, 0x000000ff, 0x000000ff, 0xfffeffff,
+    0x64b0ffff, 0x9290ffff, 0xc676ffff, 0xf36affff, 0xfe6eccff, 0xfe8170ff, 0xea9e22ff, 0xbcbe00ff, 0x88d800ff, 0x5ce430ff, 0x45e082ff,
+    0x48cddeff, 0x4f4f4fff, 0x000000ff, 0x000000ff, 0xfffeffff, 0xc0dfffff, 0xd3d2ffff, 0xe8c8ffff, 0xfbc2ffff, 0xfec4eaff, 0xfeccc5ff,
+    0xf7d8a5ff, 0xe4e594ff, 0xcfef96ff, 0xbdf4abff, 0xb3f3ccff, 0xb5ebf2ff, 0xb8b8b8ff, 0x000000ff, 0x000000ff,
+};
+
+void PPU::setPPUCtrl(uint8_t ctrl)
+{
+    baseNTAddr = 0x2000 + ((ctrl & 0x03) << 10);
+    vIncrement = ctrl & 0x04;
+    sprPTAddr = ((ctrl >> 3) & 0x01) << 12;
+    bgPTAddr = ((ctrl >> 4) & 0x01) << 12;
+    sprSize = (ctrl & 0x20) ? 16 : 8;
+    enableVblankNMI = ctrl & 0x80;
+}
+
+void PPU::setPPUMask(uint8_t mask)
+{
+    graycale = mask & 0x01;
+    showBgInLeftmost = mask & 0x02;
+    showSprInLeftmost = mask & 0x04;
+    enableBgRendering = mask & 0x08;
+    enableSprRendering = mask & 0x10;
+    emphasizeRGB = (mask >> 5) & 0x07;
+}
 
 void PPU::render()
 {
@@ -31,16 +61,10 @@ void PPU::visibleRender()
     if (cycle == 0)
         return;
 
-    if (cycle <= scanlineVisibleCycle)
+    if (cycle <= visibleCycle)
         renderPixel();
-    else if (cycle == scanlineVisibleCycle + 1)
+    else if (cycle == visibleCycle + 1)
         resetHorizontalScroll();
-    else if (cycle >= scanlineVisibleCycle + 2 && cycle <= 320)
-        evaluateSpritesForNextScanline();
-    else if (cycle >= 321 && cycle <= 336)
-        return; // TODO
-    else if (cycle <= scanlineEndCycle)
-        fetchNameTableData();
 }
 
 void PPU::renderPixel()
@@ -55,29 +79,113 @@ void PPU::renderPixel()
     int x = cycle - 1;
     int y = scanline;
 
-    fetchBackgroundPixel(x, y, bgPixel, bgOpaque);
-    fetchSpritePixel(x, y, sprPixel, sprOpaque, sprForeground);
-    compositePixel(x, y, bgPixel, sprPixel, bgOpaque, sprOpaque, sprForeground);
+    if (enableBgRendering)
+    {
+        int fine_x = (this->x + x) & 0x07;
+        if (x > 7) // (cycle 9이상일 때부터) 1사이클 당 1픽셀 출력 가능
+            renderBackgroundPixel(fine_x, y, bgPixel, bgOpaque);
+
+        if (fine_x == 7)
+            incrementHoriV();
+    }
+
+    if (enableSprRendering && x > 7)
+    {
+        renderSpritePixel(x, y, bgOpaque, sprPixel, sprOpaque, sprForeground);
+    }
+
+    // palette의 idx (< 32)
+    uint8_t pixel = compositePixel(x, y, bgPixel, sprPixel, bgOpaque, sprOpaque, sprForeground);
+    pBuffer[x][y] = colors[palette[pixel]];
 }
 
-void PPU::fetchBackgroundPixel(int x, int y, uint8_t &bgPixel, bool &bgOpaque)
+void PPU::renderBackgroundPixel(int x, int y, uint8_t &bgPixel, bool &bgOpaque)
 {
-    uint8_t fine_x = (this->fineX + x) & 0x07;
     uint8_t tile = fetchNameTableData();
     uint8_t palette = fetchAttributeTableData();
-    uint8_t plow = fetchPatternTableLow(tile, fine_x);
-    uint8_t phigh = fetchPatternTableHigh(tile, fine_x);
+    uint8_t ptLow = fetchPatternTableLow(tile, x);
+    uint8_t ptHigh = fetchPatternTableHigh(tile, x);
+    uint8_t tilePixel = (ptHigh << 1 | ptLow) & 0x03;
 
-    uint8_t tilePixel = (phigh << 1 | plow) & 0x03;
     bgPixel = ((palette & 0x03) << 2) | tilePixel;
     bgOpaque = bgPixel != 0;
-
-    if (fine_x == 7)
-        incrementHoriV();
 }
 
-void PPU::fetchSpritePixel(int x, int y, uint8_t &sprPixel, bool &sprOpaque, bool &sprForeground) {}
-void PPU::compositePixel(int x, int y, uint8_t &bgPixel, uint8_t &sprPixel, bool &bgOpaque, bool &sprOpaque, bool &sprForeground) {}
+void PPU::renderSpritePixel(int x, int y, bool bgOpaque, uint8_t &sprPixel, bool &sprOpaque, bool &sprForeground)
+{
+    /**
+     * spr:
+     * - [31:24] X좌표
+     * - [23:16] 속성(attribute) (하위 2 비트: 팔레트, 그 외 비트: 수직/수평 플립, 우선순위)
+     * - [15:8]  타일번호(tile)
+     * - [7:0]   Y좌표
+     */
+    for (uint8_t sprIdx : secondaryOAM)
+    {
+        uint32_t spr = oam[sprIdx];
+        uint8_t spry = (uint8_t)((spr >> 0) & 0xFF);
+        uint8_t tile = (uint8_t)((spr >> 8) & 0xFF);
+        uint8_t attr = (uint8_t)((spr >> 16) & 0xFF);
+        uint8_t sprx = (uint8_t)((spr >> 24) & 0xFF);
+
+        int xOffset = x - sprx;
+        int yOffset = y - spry;
+
+        if (xOffset < 0 || xOffset > 7)
+            continue;
+        if (yOffset < 0 || yOffset > sprSize)
+            continue;
+
+        bool flipVertical = attr & 0x80;
+        bool flipHorizontal = attr & 0x40;
+        bool behindBG = attr & 0x20; // Priority
+        uint8_t paletteIdx = attr & 0x03;
+
+        int tileY = flipVertical ? (sprSize - 1 - yOffset) : yOffset;
+        int tileX = flipHorizontal ? xOffset : (7 - xOffset);
+
+        uint16_t tilePTAddr;
+        if (sprSize == 16) // 8x16 스프라이트 처리
+        {
+            // TODO
+        }
+        else
+            tilePTAddr = sprPTAddr + (tile * 16) + tileY;
+
+        // fetch pt low-plain / high-plain
+        uint8_t pixel = fetchPatternTablePixelData(tilePTAddr, tileX);
+
+        if (!(sprOpaque = sprPixel))
+            continue;
+
+        sprPixel = 0x10 | (paletteIdx << 2) | (pixel & 0x03);
+        sprForeground = !behindBG;
+
+        // set sprite zero hit flag
+        // - true: spr가 0이고 현재 pixel이 0이 아니어야 함 + 배경 역시 불투명하고 렌더링 가능해야 함.
+        if (sprIdx == 0 && bgOpaque && enableBgRendering)
+            sprZeroHit = true;
+
+        // priority: 가장 낮은 인덱스를 그린다.
+        return;
+    }
+}
+
+uint8_t PPU::compositePixel(int x, int y, uint8_t bgPixel, uint8_t sprPixel, bool bgOpaque, bool sprOpaque, bool sprForeground)
+{
+    if (!bgOpaque && !sprOpaque)
+        return 0;
+
+    if (!bgOpaque && sprOpaque)
+        return sprPixel;
+
+    if (bgOpaque && !sprOpaque)
+        return bgOpaque;
+
+    if (sprForeground)
+        return sprPixel;
+    return bgPixel;
+}
 
 uint8_t PPU::fetchNameTableData()
 {
@@ -99,20 +207,28 @@ uint8_t PPU::fetchAttributeTableData()
  * - Low Plane:  8 bytes @ $0000 - $0007
  * 0 High Plane: 8 bytes @ $0008 - $000F
  */
-uint8_t PPU::fetchPatternTableLow(uint8_t tile, uint8_t fine_x)
+uint8_t PPU::fetchPatternTablePixelData(uint16_t ptAddr, uint8_t tileX)
 {
-    uint16_t ptBase = bgPage << 12; // 0x0000 or 0x1000
-    uint16_t fineY = (v >> 12) & 0x07;
-    uint16_t ptAddr = ptBase + tile * 16 + fineY;
-    return (read(ptAddr) >> (7 - fine_x)) & 0x01;
+    // uint16_t ptAddr = bgPTAddr + tile * 16 + tileY;
+    uint8_t ptLow = read(ptAddr);
+    uint8_t ptHigh = read(ptAddr + 8);
+    uint8_t lowBit = ((ptLow >> tileX) & 1) & 0x01;
+    uint8_t highBit = ((ptLow >> tileX) & 1) & 0x01;
+    return (highBit << 1) | lowBit;
 }
 
-uint8_t PPU::fetchPatternTableHigh(uint8_t tile, uint8_t fine_x)
+uint8_t PPU::fetchPatternTableLow(uint8_t tile, uint8_t tileX)
 {
-    uint16_t ptBase = bgPage << 12;
     uint16_t fineY = (v >> 12) & 0x07;
-    uint16_t ptAddr = ptBase + tile * 16 + fineY + 8;
-    return (read(ptAddr) >> (7 - fine_x)) & 0x01;
+    uint16_t ptAddr = bgPTAddr + tile * 16 + fineY;
+    return (read(ptAddr) >> (7 - tileX)) & 0x01;
+}
+
+uint8_t PPU::fetchPatternTableHigh(uint8_t tile, uint8_t tileX)
+{
+    uint16_t fineY = (v >> 12) & 0x07;
+    uint16_t ptAddr = bgPTAddr + tile * 16 + fineY + 8;
+    return (read(ptAddr) >> (7 - tileX)) & 0x01;
 }
 
 /*
